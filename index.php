5edb3c30d025af4b57346ec9cd88a7669adea402
Etienne <etdhennin@gmail.com>
2018-05-10 18:12:01 +0200

    revert index

--- index.php
+++ index.php
@@ -4,10 +4,23 @@ require 'vendor/autoload.php';
 
 use BenderBot\Bender;
 
+$availaibleConfigs = ['twitter'];
+$config            = $availaibleConfigs[0];
+
+if (isset($argc)) {
+	for ($i = 0; $i < $argc; $i++) {
+		$config = $argc[1];
+	}
+}
+
+if(!in_array($config, $availaibleConfigs)) {
+    exit('Unknown configuration' . "\n");
+}
+
 $bender    = new Bender();
 $searchFor = ['SpaceX', 'Elon', 'Musk'];
 
-if($bender->loadConfig('twitter')) {
+if($bender->loadConfig($config)) {
     $bender->initClient();
     $bender->lookFor($searchFor);
 }
