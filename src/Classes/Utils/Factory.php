37a572096637a26eafd93c80d8c87bd43b57dced
Etienne <etdhennin@gmail.com>
2018-05-11 13:58:13 +0200

    added parameters file for mysql & db infos

--- src/Classes/Utils/Factory.php
+++ src/Classes/Utils/Factory.php
@@ -2,22 +2,36 @@
 
 namespace Utils;
 
+use Tools;
+
 /**
  * Provide Db Connection, will provide bots when more config is added.
  */
 class Factory
 {
+    const CONFIG_DIRECTORY = __DIR__  . '/../../../config/';
+    const PARAMETERS_FILE  = 'parameters';
+
     private static $dbInstance;
 
     public static function getDb()
     {
         if(!isset(self::$dbInstance)) {
+
+            $dbParams = Tools::extractJsonFromFile(self::CONFIG_DIRECTORY . self::PARAMETERS_FILE);
+            $host     = $dbParams['host'];
+            $dbname   = $dbParams['name'];
+            $user     = $dbParams['user'];
+            $password = $dbParams['password'];
+
             try {
-                self::dbInstance = new PDO('mysql:host=localhost;dbname=bender;charset=utf8', 'root', '');
+                self::dbInstance = new PDO("mysql:host=$host;dbname=$name;charset=utf8", $user, $password);
             } catch (\Exception $e) {
                 exit("Failed to connect to $dbName or MySQL server");
             }
         }
+
+        return self::$dbInstance;
     }
 
 
