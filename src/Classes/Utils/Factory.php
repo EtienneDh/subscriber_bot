a88e4ddc6287fe5b5c24d18e8c0a45e9bce8c89b
Etienne <etdhennin@gmail.com>
2018-05-25 00:18:58 +0200

    removed unused code

--- src/Classes/Utils/Factory.php
+++ src/Classes/Utils/Factory.php
@@ -14,30 +14,6 @@ use BenderBot\BenderConfigurator;
  */
 class Factory
 {
-    const PARAMETERS_FILE  = 'parameters';
-
-    private static $dbInstance;
-
-    public static function getDb()
-    {
-        if(!isset(self::$dbInstance)) {
-
-            $dbParams = Tools::extractJsonFromFile(self::PARAMETERS_FILE);
-            $host     = $dbParams['host'];
-            $dbname   = $dbParams['name'];
-            $user     = $dbParams['user'];
-            $password = $dbParams['password'];
-
-            try {
-                self::$dbInstance = new PDO("mysql:host=$host;dbname=$name;charset=utf8", $user, $password);
-            } catch (\Exception $e) {
-                exit("Failed to connect to $dbName or MySQL server \n");
-            }
-        }
-
-        return self::$dbInstance;
-    }
-
     public static function getBender(string $appName) : AbstractBender
     {
         $benderBot = new Bender();
@@ -48,5 +24,4 @@ class Factory
 
         return $benderBot;
     }
-
 }
